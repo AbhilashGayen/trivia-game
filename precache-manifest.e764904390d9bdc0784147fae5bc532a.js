8e0a3dd4b4dfd3f6f71675d2e4fde834629f31cc
Abhilash Gayen <abhilashgayen@Abhilashs-MacBook-Pro.local>
2020-06-06 19:40:51 +0530

    Updates

---
+++ precache-manifest.e764904390d9bdc0784147fae5bc532a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc276e005ffacda2077267cd4b773e1c",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "a76b63aada97e3146a91",
+    "url": "/trivia/static/css/main.dbcec4a3.chunk.css"
+  },
+  {
+    "revision": "9da8e785c2d5dfa931cd",
+    "url": "/trivia/static/js/2.661cf3df.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/trivia/static/js/2.661cf3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76b63aada97e3146a91",
+    "url": "/trivia/static/js/main.bda9232f.chunk.js"
+  },
+  {
+    "revision": "fc524abff776149b632a",
+    "url": "/trivia/static/js/runtime-main.85d2a6a6.js"
+  },
+  {
+    "revision": "316c232187bac46e708fad0b5d83666f",
+    "url": "/trivia/static/media/Quicksand-Regular.316c2321.ttf"
+  }
+]);
